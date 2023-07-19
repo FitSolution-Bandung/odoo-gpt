32168c6d73e3e6869b74f5102d9aef8778edebc1
Andhitiawarman Nugraha <andhitiawarman@gmail.com>
2023-07-19 15:33:01 +0700

    15.32

---
+++ login.py
@@ -0,0 +1,117 @@
+import streamlit as st
+import xmlrpc.client
+import sqlite3
+import hashlib
+from cryptography.fernet import Fernet
+from token_verification import get_credentials
+import token_verification
+from datetime import datetime, timedelta
+from urllib.parse import urlparse
+from socket import gaierror
+
+key = b'4Gpyw4r57coCTSULSqGcq2ywpECnRK3fkAHcJvWqc08='
+cipher_suite = Fernet(key)
+
+
+def verify_user(url, db, username, password):
+    common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+    uid = common.authenticate(db, username, password, {})
+
+    if uid:
+        return uid
+    else:
+        return None
+
+
+def drop_table(db_name, table_name):
+    con = sqlite3.connect(db_name)
+    cursor = con.cursor()
+    
+    cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
+    con.commit()
+    
+    print(f"Table '{table_name}' has been dropped.")
+        
+    con.close()
+
+
+
+def store_credentials(url, db, username, password):
+    token = hashlib.sha256((url + db + username + password).encode()).hexdigest()
+
+    password_encrypted = cipher_suite.encrypt(password.encode())
+    
+    # Simpan waktu saat ini dalam format string
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    conn = sqlite3.connect('user_data.db')
+    c = conn.cursor()
+
+    # Cek apakah record dengan url, db, dan username yang sama sudah ada
+    c.execute("SELECT * FROM users WHERE url = ? AND db = ? AND username = ?", (url, db, username))
+    record = c.fetchone()
+
+    if record is None:
+        # Jika record belum ada, buat record baru
+        c.execute("INSERT INTO users (url, db, username, password, token, created_at) VALUES (?, ?, ?, ?, ?, ?)", 
+                  (url, db, username, password_encrypted, token, now))
+    else:
+        # Jika record sudah ada, update record
+        c.execute("UPDATE users SET password = ?, token = ?, created_at = ? WHERE url = ? AND db = ? AND username = ?", 
+                  (password_encrypted, token, now, url, db, username))
+
+    conn.commit()
+    conn.close()
+
+    return token
+
+
+def is_valid_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+ 
+ 
+
+if 'token' not in st.session_state or not st.session_state['token']:
+
+    st.markdown("""
+                # Login to Odoo
+                """)
+
+    with st.form(key='login_form'):
+        url = st.text_input("Odoo URL", value='https://erp.fujicon-japan.com')
+        db = st.text_input("Database", value='erp')
+        username = st.text_input("Username", value='andhi@fujicon-japan.com')
+        password = st.text_input("Password", type="password")
+        submit_button = st.form_submit_button(label='Login')
+
+        
+        if submit_button and is_valid_url(url) and db and username and password:
+            try:
+                user_id = verify_user(url, db, username, password)
+                if user_id:
+                    st.success("Login successful!")
+                    token = store_credentials(url, db, username, password)
+                    st.session_state['token'] = token
+                    st.write("Your token: ", token)
+                    st.session_state['logged_in'] = True
+                    st.experimental_rerun()
+                    
+                
+            
+                else:
+                    st.error("Login failed. Please check your credentials.")
+                    st.session_state['token'] = None
+
+            except gaierror:
+                st.error("Failed to connect to the specified URL. Please check the URL and try again.")
+
+
+if 'logged_in' in st.session_state and st.session_state['logged_in']:
+    token_verification.run()
+    
+  
---
+++ token_verification.py
@@ -0,0 +1,126 @@
+import sqlite3
+from cryptography.fernet import Fernet
+import streamlit as st  
+from datetime import datetime, timedelta
+
+# Anda harus mengubah ini dengan kunci yang Anda gunakan untuk enkripsi password
+key = b'4Gpyw4r57coCTSULSqGcq2ywpECnRK3fkAHcJvWqc08='
+
+cipher_suite = Fernet(key)
+
+def get_credentials(token):
+    conn = sqlite3.connect('user_data.db')
+    c = conn.cursor()
+    c.execute("SELECT url, username, password, created_at FROM users WHERE token = ?", (token,))
+    result = c.fetchone()
+    conn.close()
+
+    if result:
+        url, username, password_encrypted, created_at = result
+        created_at = datetime.strptime(created_at, "%Y-%m-%d %H:%M:%S")
+        
+        # Check if the token has expired
+        if datetime.now() - created_at > timedelta(days=5):
+            print('Token has expired')
+            return None
+
+        # Decrypt password
+        password = cipher_suite.decrypt(password_encrypted).decode()
+
+        return url, username, password, created_at
+
+    else:
+        print(f'No user found with token: {token}')
+        return None
+    
+    
+#Lihat isi  database di sqlite
+def view_tables(db_name):
+    con = sqlite3.connect(db_name)
+    cursor = con.cursor()
+    
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    
+    for table in tables:
+        st.write(f"Table name: {table[0]}")
+        
+    con.close()
+
+#Lihat isi table
+def view_table(db_name, table_name):
+    con = sqlite3.connect(db_name)
+    cursor = con.cursor()
+    
+    cursor.execute(f"SELECT * FROM {table_name};")
+    rows = cursor.fetchall()
+    
+    for row in rows:
+        st.write(row)
+        
+    con.close()
+
+
+
+#Hapus semua isi table
+def clear_table(db_name, table_name):
+    con = sqlite3.connect(db_name)
+    cursor = con.cursor()
+
+    cursor.execute(f"DELETE FROM {table_name};")
+    con.commit()
+    
+    print("All rows deleted from table 'user'")
+    
+    con.close()
+
+
+
+
+def run():
+    
+    st.markdown("""
+        # You are loged in.
+                    
+        :information_source: Your token: `{}`
+        Please click the button below to go to the Token Verification Page.
+        """.format(st.session_state['token']), unsafe_allow_html=True)
+        
+
+    # Formulir input token
+    with st.form(key='token_form'):
+        input_token = st.text_input("Enter your token")
+        submit_token_button = st.form_submit_button(label='Submit Token')
+        
+        if submit_token_button and input_token:
+            credentials = get_credentials(input_token)
+            
+            if credentials:
+                url, username, password, created_at = credentials
+                
+                # Calculate remaining time
+                remaining_time = created_at + timedelta(days=5) - datetime.now()
+                remaining_hours = remaining_time.total_seconds() // 3600
+                
+                st.write(f"URL: {url}")
+                st.write(f"Username: {username}")
+                st.write(f"Password: {password}")
+                st.write(f"Token will expire in {remaining_hours} hours")
+                st.write('---')
+
+                #tampilkan isi table
+                with st.expander("Table: 'users'"):
+                    view_table('user_data.db', 'users')
+            
+            else:
+                st.error("Invalid token. Please check your token.")
+
+           
+
+    # Tombol logout
+    logout_button = st.button('Logout')
+    if logout_button:
+        st.session_state['token'] = None
+        st.session_state['logged_in'] = False
+        st.experimental_rerun() # Refresh halaman
+
