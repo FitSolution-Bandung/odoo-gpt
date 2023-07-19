f4f0dc3c0b7b23f9afbdc10af3fe0e2a98b53260
Andhitiawarman Nugraha <andhitiawarman@gmail.com>
2023-07-19 11:00:33 +0700

    update 19/07

--- chat_app.py
+++ chat_app.py
@@ -10,56 +10,52 @@ import os
 #test keterangan tambahan
 
 def chat_app():
-	
-	
-	#BODY
-	# Set up the Streamlit app layout
-	st.title("🤖 Odoo-GPT")
-	st.subheader("Integrasi Odoo-Whatsapp dengan optimalisasi GPT-3.5")
-	
-	# Ask the user to enter their OpenAI API key
-	API_O = st.sidebar.text_input("API-KEY", type="password")
-	
-	if API_O == "":
-	  # API_O = os.environ['OPENAI_KEY']
-	  
-	  API_O = os.environ['OPENAI_KEY']
-	  st.info(f"""
-				Saat ini menggunakan API-KEY default.             
-				""")
-	
-	with st.expander("📝 ENTITY_MEMORY_CONVERSATION_TEMPLATE", expanded=False):
-	  st.write(ENTITY_MEMORY_CONVERSATION_TEMPLATE)
-	  st.write(ConversationEntityMemory)
-	
-	# Session state storage would be ideal
-	if API_O:
-	  # Create an OpenAI instance
-	  llm = ChatOpenAI(temperature=0,
-	               openai_api_key=API_O,
-	               model_name=MODEL,
-	               verbose=False)
-	
-	  # Create a ConversationEntityMemory object if not already created
-	  if 'entity_memory' not in st.session_state:
-	    st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K)
-	
-	  # Create the ConversationChain object with the specified configuration
-	  Conversation = ConversationChain(llm=llm,
-	                                   prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
-	                                   memory=st.session_state.entity_memory)
-	else:
-	  st.sidebar.warning(
-	    'API key required to try this app.The API key is not stored in any form.')
-	  # st.stop()
-	
-	#Kirim Pesan
-	chat_history_expander = st.expander("💬 Chat History", expanded=True)
-	chat_history_expander.empty()
-	
-	user_input = st.chat_input(placeholder="Ketik pesan disini ...")
-	
-	#with st.expander("💬 Chat History", expanded=True):
-	with chat_history_expander:
-	  if user_input:
-	    show_chat_histories(user_input)
+  
+  
+  #BODY
+  # Set up the Streamlit app layout
+  st.title("🤖 Odoo-GPT")
+  st.subheader("Integrasi Odoo-Whatsapp dengan optimalisasi GPT-3.5")
+  
+  # Ask the user to enter their OpenAI API key
+  API_O = st.sidebar.text_input("API-KEY", type="password")
+  
+  if API_O == "":
+    # API_O = os.environ['OPENAI_KEY']
+    
+    API_O = os.environ['OPENAI_KEY']
+    st.info(f"""
+        Saat ini menggunakan API-KEY default.             
+        """)
+  
+  with st.expander("📝 ENTITY_MEMORY_CONVERSATION_TEMPLATE", expanded=False):
+    st.write(ENTITY_MEMORY_CONVERSATION_TEMPLATE)
+    st.write(ConversationEntityMemory)
+  
+  # Session state storage would be ideal
+  if API_O:
+    # Create an OpenAI instance
+    llm = ChatOpenAI(temperature=0,
+                 openai_api_key=API_O,
+                 model_name=MODEL,
+                 verbose=False)
+  
+    # Create a ConversationEntityMemory object if not already created
+    if 'entity_memory' not in st.session_state:
+      st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K)
+  
+    # Create the ConversationChain object with the specified configuration
+    Conversation = ConversationChain(llm=llm,
+                                     prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
+                                     memory=st.session_state.entity_memory)
+  else:
+    st.sidebar.warning(
+      'API key required to try this app.The API key is not stored in any form.')
+    # st.stop()
+  
+  #Kirim Pesan
+  chat_history_expander = st.expander("💬 Chat History", expanded=True)
+  chat_history_expander.empty()
+  
+  user_input = st.chat_input(placeholder="Ketik pesan disini ...")
+  
--- main.py
+++ main.py
@@ -5,8 +5,6 @@ Dibangun dengan pustaka Streamlit, Langchain, dan llms, skrip ini memungkinkan i
 berbasis percakapan yang kontekstual dan informatif antara sistem Odoo dan pengguna melalui WhatsApp, 
 dengan asisten AI memfasilitasi dan memperkaya proses komunikasi.# Author: Avratanu Biswas
 
-
-
 # July 2023
 #by Andhitiawarman Nugraha
 """
@@ -16,12 +14,8 @@ import streamlit as st
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationEntityMemory
 from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
-# from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
 import os
-from chat_app import chat_app 
-
-
 
 
 
@@ -61,29 +55,31 @@ def new_chat():
 
 
 def show_chat_histories(user_input):
+    
+    past = st.session_state["past"]
+    generated = st.session_state["generated"]
 
-  output = Conversation.run(input=user_input)
+    if len(past) > 0:
+        for i in range(0, len(generated)):
+            with st.chat_message(name="User", avatar="🧑‍💻"):
+                st.write(past[i])
 
-  st.session_state["past"].append(user_input)
-  st.session_state["generated"].append(output)
+            with st.chat_message(name="Odoo-GPT", avatar="🤖"):
+                    st.write(generated[i])
+ 
 
-  past = st.session_state["past"]
-  generated = st.session_state["generated"]
-  download_str = st.session_state["download_str"]
+    with st.chat_message(name="User", avatar="🧑‍💻"):
+       st.write(user_input)
 
-  user = "user"
-  bot = "Odoo-GPT"
+    with st.chat_message(name="Odoo-GPT", avatar="🤖"):
+        with st.spinner("Memuat Respon ..."):
+            output = Conversation.run(input=user_input)
 
-  if len(past) > 0:
-    for i in range(0, len(past)):
-      with st.chat_message(user):
-        st.write(past[i])
+        st.write(output)
 
-      with st.chat_message(bot):
-        st.write(generated[i])
-
-  else:
-    st.info("No conversation history yet.")
+    st.session_state["past"].append(user_input)
+    st.session_state["generated"].append(output)
+ 
 
 
 #SIDEBAR
@@ -108,13 +104,57 @@ with st.sidebar.expander("🛠️ ", expanded=False):
                       min_value=3,
                       max_value=1000)
 
+#BODY
+# Set up the Streamlit app layout
+st.title("🤖 Odoo-GPT")
+st.subheader("Integrasi Odoo-Whatsapp dengan optimalisasi GPT-3.5")
 
+# Ask the user to enter their OpenAI API key
+API_O = st.sidebar.text_input("API-KEY", type="password")
 
+if API_O == "":
+  # API_O = os.environ['OPENAI_KEY']
+  
+  API_O = os.environ['OPENAI_KEY']
+  st.info(f"""
+      Saat ini menggunakan API-KEY default.             
+      """)
 
-body = chat_app()
+with st.expander("📝 ENTITY_MEMORY_CONVERSATION_TEMPLATE", expanded=False):
+  st.write(ENTITY_MEMORY_CONVERSATION_TEMPLATE)
+  st.write(ConversationEntityMemory)
 
+# Session state storage would be ideal
+if API_O:
+  # Create an OpenAI instance
+  llm = OpenAI(temperature=0,
+               openai_api_key=API_O,
+               model_name=MODEL,
+               verbose=False)
 
+  # Create a ConversationEntityMemory object if not already created
+  if 'entity_memory' not in st.session_state:
+    st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K)
 
+  # Create the ConversationChain object with the specified configuration
+  Conversation = ConversationChain(llm=llm,
+                                   prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
+                                   memory=st.session_state.entity_memory)
+else:
+  st.sidebar.warning(
+    'API key required to try this app.The API key is not stored in any form.')
+  # st.stop()
+
+#Kirim Pesan
+chat_history_expander = st.expander("💬 Chat History", expanded=True)
+chat_history_expander.empty()
+
+user_input = st.chat_input(placeholder="Ketik pesan disini ...")
+
+#with st.expander("💬 Chat History", expanded=True):
+with chat_history_expander:
+  if user_input:
+    show_chat_histories(user_input)
 
 #SIDEBAR
 # Display stored conversation sessions in the sidebar
