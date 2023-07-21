2fbe7c99b77b807eed6e3636b421034ce14d1947
Andhitiawarman Nugraha <andhitiawarman@gmail.com>
2023-07-21 16:17:19 +0700

    update about

--- pages/5_ℹ_About.py
+++ pages/5_ℹ_About.py
@@ -13,12 +13,8 @@ def check_server(address, port):
         # Try to connect
         s.connect((address, port))
         print(f"Connected to {address} on port {port}")
-
-        
-
-
-
         return True
+
     except socket.error as e:
         print(f"Connection to {address} on port {port} failed: {e}")
         return False
@@ -26,21 +22,13 @@ def check_server(address, port):
         s.close()
 
 
-
-
-
 #tampilkan localhost:80 dengan component html
-url = 'http://localhost:80'
-# components.iframe(url)\
-st.components.v1.iframe(url, height=500, scrolling=True)
+# url = 'http://localhost:8500'
+# # components.iframe(url)\
+# st.components.v1.iframe(url, height=500, scrolling=True)
 
-
-
-
-
-
-# if 'entity_memory' not in st.session_state:
-
+ 
+#check apakah sudah login
 if 'logged_in' in st.session_state and st.session_state['logged_in']:
     status_login = st.session_state['logged_in']
     
@@ -49,40 +37,18 @@ else:
 
 
 
-
- # Membuat socket
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     # Bind socket ke alamat dan port
-#     s.bind(('localhost', 80))
-#     # Mulai mendengarkan koneksi
-#     s.listen()
-    
-#     # Menerima koneksi
-#     conn, addr = s.accept()
-
-#     with conn:
-#         print(f"Connected by {addr}")
-#         while True:
-#             # Menerima data dari client
-#             data = conn.recv(1024)
-#             # Jika tidak ada data, hentikan loop
-#             if not data:
-#                 break
-#             # Tampilkan data yang diterima
-#             st.write(f"Received: {data.decode('utf-8')}")
-#             # Kirim balasan ke client
-#             conn.sendall(data)
-
-
-
-server_running = check_server('127.0.0.1', 80)
+#check apakah server sudah running
+address = '127.0.0.1'
+port = 8500
+server_running = check_server(address, port)
     
 if server_running:
-    st.success('Server is running on port 80')
+    st.success(f'Server **{address}** is running on port **{port}**')
 else:
     st.error('Server is not running on port 80')
 
 
+#about odoo-gpt
 st.markdown("""
 # About Odoo-GPT
 
