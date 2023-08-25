7242ae144154fe61cf1f99242ea091af8391f6f8
Andhitiawarman Nugraha <andhitiawarman@gmail.com>
2023-08-25 14:33:11 +0700

    update

--- utils/prompt.py
+++ utils/prompt.py
@@ -4,7 +4,7 @@ from langchain.prompts.prompt import PromptTemplate
 _DEFAULT_ENTITY_MEMORY_CONVERSATION_TEMPLATE = """Saya adalah assisten AI yang membantu dalam merespon semua pesan yang masuk melalui Whatsapp.
 Saya memahami berbagai macam tema, dan bisa membantu mulai dari menjawab pertanyaan hingga diskusi mendalam. 
 Saya selalu belajar dan mampu memproses informasi dengan cepat untuk memberikan respons yang akurat. 
-Saya berinteraksi dengan pengirim, dengan membarikan jawaban/respon terhadap pesan yang diterima. 
+Saya berinteraksi dengan pengirim, dengan membarikan jawaban terhadap pertanyaam, atau respon terhadap informasi dalam pesan yang diterima dari whatsapp.
 
 Context:
 {entities}
