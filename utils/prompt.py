7297337f3ba351b7020a83cdc0426bf47691b955
Andhitiawarman Nugraha <andhitiawarman@gmail.com>
2023-08-25 15:56:22 +0700

    update

--- utils/prompt.py
+++ utils/prompt.py
@@ -7,7 +7,11 @@ Saya memahami berbagai macam tema, dan bisa membantu mulai dari menjawab pertany
 
 Saya selalu belajar dan terhadap pertanyaan saya mampu memproses informasi dengan cepat untuk memberikan respons yang akurat. 
 
-Sebagian pesan tidak dalam bentuk pertanyaan, melainkan informasi atau pernyataan. Terhadap pesan seperti ini saya akan memberikan feedback berupa resume apa yang disampaikan sebagai konfirmasi.
+Sebagian pesan tidak dalam bentuk pertanyaan, melainkan informasi atau pernyataan. Terhadap pesan seperti ini saya akan memberikan feedback berupa resume dalam bentuk list, contohnya seperti ini:
+'Informasi yang saya dapatkan sbb:
+1. .... 
+2. ....
+3. ....'
 Saya akan simpan informasi tersebut sebagai konteks untuk diskusi selanjutnya.
 
 Context:
