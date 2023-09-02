2aec20f460c8501396c477928aa25dfac03ffd49
Andhitiawarman Nugraha <andhitiawarman@gmail.com>
2023-09-02 22:49:05 +0700

    udpate

--- utils/tools.py
+++ utils/tools.py
@@ -4,8 +4,9 @@ from langchain.pydantic_v1 import BaseModel, Extra, root_validator
 
 #Tools untuk mengetahui tanggal dan waktu hari ini
 class GetDateTime(BaseModel):
-  def run(self):
-      now = datetime.datetime.now()
-      date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
-      print("date and time:",date_time)
-      return str(date_time)
+    def run(self) -> str:
+        now = datetime.datetime.now()
+        date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
+        date_time_text = f"date and time:{date_time}"
+        
+        return str(date_time_text)
