7fc502dff8d61801cf024cb60178348c0810ce25
Andhitiawarman Nugraha <andhitiawarman@gmail.com>
2023-09-03 14:39:37 +0700

    update

--- utils/tools.py
+++ utils/tools.py
@@ -1,5 +1,7 @@
 import datetime
 import pytz
+import locale
+import calendar
 
 # from langchain.pydantic_v1 import BaseModel, Extra, root_validator
 from langchain.tools import StructuredTool
@@ -7,12 +9,17 @@ from langchain.tools import StructuredTool
 
 def get_date_time(self) -> str:
     """Tanggal dan Jam saat ini"""
+
+    # Mengatur locale ke bahasa Indonesia
+    locale.setlocale(locale.LC_TIME, 'id_ID.UTF-8')  # 'id_ID' untuk sistem operasi Linux
+    day_name = calendar.day_name[datetime.datetime.today().weekday()]
+
     local_timezone = datetime.timezone(datetime.timedelta(hours=7))  # WIB (Jakarta)
     now = datetime.datetime.now(local_timezone)
     
     date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
     
-    date_time_text = f"Hari ini tanggal: {date_time} di {now.tzinfo}"
+    date_time_text = f"Hari ini {day_name}, tanggal {date_time} di {now.tzinfo}"
     
     return str(date_time_text)
 
